6448581eaaf784f6c005d254099b6353e4cb00b4
Anton Ruban <anton.ruban@chisw.com>
2017-09-04 17:31:13 +0300

    Add json-like routing error.

--- app/api.rb
+++ app/api.rb
@@ -6,4 +6,9 @@ class API < Grape::API
   format :json
 
   mount V1::FakeEndpoint
+
+  # If requested route was not found, return the client error.
+  route :any, '*path' do
+    error!({ message: 'Route not found' }, 404)
+  end
 end
