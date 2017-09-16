a57f59acbbd7ad37ce54ede0db28e2ade4d7b118
antnruban <antnruban@gmail.com>
2017-09-16 21:55:55 +0300

    Sequel model settings.

--- models/fish_card.rb
+++ models/fish_card.rb
@@ -1,14 +1,5 @@
 # frozen_string_literal: true
 
-# class FishCard < ActiveRecord::Base
-#   before_create :hi
-#   after_create :bye
-#
-#   def hi
-#     puts 'HI'
-#   end
-#
-#   def bye
-#     puts 'BYE'
-#   end
-# end
+class FishCard < Sequel::Model
+  include FishCards::Repository
+end
