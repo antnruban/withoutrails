e15e52d44d90b0267e16714c6b74bfaa128a168e
antnruban <antnruban@gmail.com>
2017-09-19 11:52:49 +0300

    Remove AR schema.

--- models/fish_card.rb
+++ models/fish_card.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class FishCard < Sequel::Model
+class FishCard < Sequel::Model(:fish_cards)
   include FishCards::Modules::Repository
   extend FishCards::Modules::SearchMethods
 end
